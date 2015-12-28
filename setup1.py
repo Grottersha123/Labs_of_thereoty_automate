9bb825eb7dfbed4e81d4c49c6c0958fb012757f7
Grottersha123 <grottersha123@gmail.com>
2015-12-28 23:46:31 +0300

    Последняя верся лабы с транслитом уже

--- setup1.py
+++ setup1.py
@@ -1,8 +1,14 @@
 from cx_Freeze import setup, Executable
+import sys
 
+exe = Executable(
+    script="Program_roman_number.py",
+    base="Win32GUI",
+    )
+    
 setup(
     name = "Program_roman_number",
     version = "0.1",
     description = "Translit",
-    executables = [Executable("Program_roman_number.py")]
+    executables = [exe]
 )
